67027742ee8f024575eecc0140084e89656a1293
amal a.s <amalsunil863@gmail.com>
2023-03-07 16:47:42 +0530

    first commit

---
+++ src/components/ReactCarousel.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import SimpleImageSlider from "react-simple-image-slider";
+
+const images = [
+  {
+    url: "https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1585412727339-54e4bae3bbf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1585412727339-54e4bae3bbf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+];
+
+const ReactCarousel = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <SimpleImageSlider
+        width={896}
+        height={504}
+        images={images}
+        showBullets={false}
+        showNavs={true}
+        autoPlay
+      />
+    </div>
+  );
+};
+
+export default ReactCarousel;
